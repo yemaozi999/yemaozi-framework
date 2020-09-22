021fe1f8765dc6c519866b33fc55c10a19515481
yemaozi999 <yemaozi999@qq.com>
2020-09-22 13:36:56 +0800

    auto dispatch fix bug

--- config/database.php
+++ config/database.php
@@ -26,7 +26,7 @@ return [
             // 数据库编码默认采用utf8
             'charset'  => 'utf8',
             // 数据库表前缀
-            'prefix'   => 'fm_',
+            //'prefix'   => 'fm_',
             // 数据库调试模式
             'debug'    => true,
         ],
--- framework/dispatcher/AutoDispatcher.php
+++ framework/dispatcher/AutoDispatcher.php
@@ -19,34 +19,32 @@ class AutoDispatcher implements BaseDispatcher
         public function __construct(){
 
             if (!isset($_SERVER['PATH_INFO']) && isset($_SERVER['ORIG_PATH_INFO'])){
-                $_SERVER['PATH_INFO'] = $_SERVER['ORIG_PATH_INFO'];
+                $path_info = $_SERVER['ORIG_PATH_INFO'];
+            }else{
+                $path_info = $_SERVER['REQUEST_URI'];
             }
-            $path_info = $_SERVER['PATH_INFO'];
             $app_config = Config::get('app');
             if($path_info=='index.php'){
                 $path_info=$app_config['default_app'];
             }
             $path_exp = explode(".",$path_info);
             $path = array_shift($path_exp);
+
             $path_array = explode('/',trim($path,"/"));
             $this->app_name = $path_array[0]??$app_config['default_app'];
             $this->controller_name = $path_array[1]??$app_config['default_controller'];
             $this->action_name = $path_array[2]??$app_config['default_action'];
-
         }
 
         public function run(){
-            /*$uri = $_SERVER['REQUEST_URI'];
-            if (false !== $pos = strpos($uri, '?')) {
-                $uri = substr($uri, 0, $pos);
-            }
-            $uri = rawurldecode($uri);*/
-            $class_mix = 'app\\'.$this->app_name.'\controller'.'\\'.$this->controller_name;
+            $class_mix = 'app'.DIRECTORY_SEPARATOR.$this->app_name.DIRECTORY_SEPARATOR.'controller'.DIRECTORY_SEPARATOR.$this->controller_name;
+
             if(class_exists($class_mix)){
                 define("APP_NAME",$this->app_name);
                 define("CONTROLLER_NAME",$this->controller_name);
                 define('ACTION_NAME',$this->action_name);
                 $class = new $class_mix();
+
                 if(method_exists($class,$this->action_name)){
                     call_user_func([$class,$this->action_name]);
                 }else{
